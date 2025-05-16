20fbf8cb0d93e4cf9141de3cdfc6abf945317edd
Cory <70068963+TwCory@users.noreply.github.com>
2025-05-16 14:28:25 -0400

    Create if_vdev.h

---
+++ src/include/olsn/if_vdev.h
@@ -0,0 +1,12 @@
+#ifndef OLSN_IF_VDEV_H
+#define OLSN_IF_VDEV_H
+
+typedef enum if_vdev_type
+{
+  IF_VDEV_TYPE_LOOPBACK,
+  IF_VDEV_TYPE_VLAN,
+  IF_VDEV_TYPE_VXLAN,
+
+} if_vdev_type;
+
+#endif /* OLSN_IF_VDEV_H */
