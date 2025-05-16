0aa2b3705f3a82e819af4998bc0b2b47610ea938
Cory <70068963+TwCory@users.noreply.github.com>
2025-05-16 18:10:43 -0400

    Create main.c

---
+++ src/core/main.c
@@ -0,0 +1,13 @@
+// OLSN Daemon Main Routine
+
+#include <olsn/core.h>
+#include <olsn/list.h>
+#include <olsn/string.h>
+#include <olsn/types.h>
+#include <olsn/version.h>
+
+int main(int argc, char **argv)
+{
+    olsn_init();
+    return 0;
+}
